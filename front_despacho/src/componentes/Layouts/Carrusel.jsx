0116a137fb31c22d0caf9439bcb52ae4f9236f5e
Papitas lays sabor original <al.retamalo@duocuc.cl>
2026-06-17 18:26:48 -0400

    test

--- front_despacho/src/componentes/Layouts/Carrusel.jsx
+++ front_despacho/src/componentes/Layouts/Carrusel.jsx
@@ -9,7 +9,7 @@ function Carrusel() {
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
           <img src="" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
         </div>
-        {/* Item 2 */}
+        {/* Item 2 holaaa */}
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
           <img src="/docs/images/carousel/carousel-2.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
         </div>
